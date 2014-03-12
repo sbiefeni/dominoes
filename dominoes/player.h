c78dd6c29b6ef5aa02ee1df742e44ab1e833e09e
sbiefeni <sbiefeni@gmail.com>
2014-03-11 17:20:43 -0700

    ddasd

---
+++ dominoes/player.h
@@ -0,0 +1,18 @@
+//
+//  player.h
+//  dominoes
+//
+//  Created by Stefano on 3/11/14.
+//  Copyright (c) 2014 Abstractions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "domMyScene.h"
+
+@interface player : NSObject
+
+@property swipeDirection lastSwipe;
+@property swipeDirection    currentDirection;
+
+
+@end
---
+++ dominoes/player.m
@@ -0,0 +1,13 @@
+//
+//  player.m
+//  dominoes
+//
+//  Created by Stefano on 3/11/14.
+//  Copyright (c) 2014 Abstractions. All rights reserved.
+//
+
+#import "player.h"
+
+@implementation player
+
+@end
